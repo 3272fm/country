f1a773c6c277705836858f65d0877942cd1a749d
3272fm <98327388+3272fm@users.noreply.github.com>
2022-02-22 14:20:09 +0300

    Add files via upload

---
+++ COUNTRY LIBRARY MANAGEMENT SYSTEM/main.c
@@ -0,0 +1,27 @@
+/*
+Project: COUNTRY LIBRARY MANAGEMENT SYSTEM
+Author: Fataal Muthoni
+Date: Feb,2022
+Complier:GNU GCC
+Language:C99
+License:MIT
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tCountry Library\n");
+    printf("Welcome Miss.Fataal.\n");
+    printf("What would you like to do?\n");
+    printf("1. Add a new user\n");
+    printf("2. Add a book.at\n");
+    printf("Selected action:\n");
+
+
+
+
+
+
+    return 0;
+}
